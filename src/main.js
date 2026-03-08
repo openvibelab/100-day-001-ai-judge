15208fd8fb5533686fd632b420375a6662cec0c1
openvibelab <alyicabhz@gmail.com>
2026-03-09 00:39:49 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { inject } from '@vercel/analytics'
 import App from './App.vue'
 import './style.css'
 
@@ -19,3 +20,5 @@ const router = createRouter({
 })
 
 createApp(App).use(router).mount('#app')
+
+inject()
